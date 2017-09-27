dff01f4fbbdf0a98148617b0e92548f3a3aff97d
Bob Kwiencien <bkwiencien@gmail.com>
2017-09-27 06:22:37 -0400

    added constructors

---
+++ BasicCard.js
@@ -0,0 +1,5 @@
+function BasicCard(pfront,pback) {
+	this.front = pfront;
+	this.back  = pback;
+}
+module.exports = BasicCard;
---
+++ ClozeCard.js
@@ -0,0 +1,5 @@
+function ClozeCard(text,cloze) {
+    this.fullText = text
+    this.cloze = cloze;
+}
+module.exports = ClozeCard;
