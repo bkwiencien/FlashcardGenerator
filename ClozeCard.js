4a2098b93c6012aa66d9949ee8b8812563330d42
Bob Kwiencien <bkwiencien@gmail.com>
2017-09-27 10:51:35 -0400

    cloze file write

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,7 +1,21 @@
 var fs = require("fs");
 module.exports = ClozeCard;
-function ClozeCard(text,cloze) {
+function ClozeCard(text,cloze,partial) {
     this.fullText = text
     this.cloze = cloze;
+    this.partial = partial;
     this.type  = "clozen";
+    var data = {
+            fullText: this.fullText,
+            cloze: this.cloze,
+            partial: this.partial,
+            type: "clozen",
+        };
+    this.logit = function() {
+        fs.appendFile("log.txt", JSON.stringify(data) + ';\n', "utf8", function(error) {
+            if (error) {
+                console.log(error);
+            }
+        });
+    };
 }
